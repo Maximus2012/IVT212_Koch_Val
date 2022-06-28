5e629d2d3dcd5bddaa0aa088e01542eb6a307dec
IVT212KirillValeev <107980364+IVT212KirillValeev@users.noreply.github.com>
2022-06-28 17:21:58 +0600

    Unit тесты вплоть до ctg

---
+++ calcTest/resultCalcTest.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Calculate;
+
+namespace calcTest
+{
+
+   
+
+
+
+    [TestClass]
+    public class resultCalcTest
+    {
+        [TestMethod]
+        public void TestMethodPlus()
+        {
+            fCalc calc = new fCalc();
+            double a = 5; double b = 10;
+            double expected = 15;
+            double result;
+            result = calc._bRavno_Click(a, b, 1);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodMinus()
+        {
+            fCalc calc = new fCalc();
+            double a = 10; double b = 5;
+            double expected = 5;
+            double result;
+            result = calc._bRavno_Click(a, b, 2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodMultiply()
+        {
+            fCalc calc = new fCalc();
+            double a = 5; double b = 10;
+            double expected = 50;
+            double result;
+            result = calc._bRavno_Click(a, b, 3);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodDivide()
+        {
+            fCalc calc = new fCalc();
+            double a = 10; double b = 5;
+            double expected = 2;
+            double result;
+            result = calc._bRavno_Click(a, b, 4);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodPow()
+        {
+            fCalc calc = new fCalc();
+            double a = 5; double b = 10;
+            double expected = Math.Pow(a, b);
+            double result;
+            result = calc._bRavno_Click(a, b, 5);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodSin()
+        {
+            Operations operation = new Operations();
+            double a = 1;
+            double expected = 0.8415; double result;
+            result = operation.sin(a);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodCos()
+        {
+            Operations operation = new Operations();
+            double a = 1;
+            double expected = 0.5403; double result;
+            result = operation.cos(a);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodTan()
+        {
+            Operations operation = new Operations();
+            double a = 1;
+            double expected = 1.5574; double result;
+            result = operation.tan(a);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethodCot()
+        {
+            Operations operation = new Operations();
+            double a = 0.78539;
+            double expected = 1; double result; double perem;
+            perem = operation.tan(a);
+            result = 1 / perem;
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    
+}
